35a1ab93f8d9685fc4dad077cb2f049dfbb7e0b6
Xdestiny <bitzyw@126.com>
2018-04-26 22:32:32 +0800

    [add] 添加CART回归树

--- TreeNode.py
+++ TreeNode.py
@@ -6,12 +6,15 @@ class TreeNode:
         self.parent = None
         self.child = []
         self.data = []
+        self.depth = 0
     def addChild(self, node):
         self.child.append(node)
         node.parent = self
+        node.depth = self.depth+1
     def removeChild(self, node):
         self.child.remove(node)
         node.parent = None
+        node.depth = 0
 
 class DecisionTreeNode(TreeNode):
     # data中第一个值保存的为对应label
@@ -37,6 +40,20 @@ class DecisionTreeNode(TreeNode):
             nodes.remove(nodes[0])
 
 class RegressionTreeNode(TreeNode):
+    # data中第一个值对应结果是多少
+    # child有值时，data第二个值代表处理的是第几个feature，第三个值代表在该feature上的切分点是多少
+    # 保证左子节点是小于切分点，大于切分店是在右子节点
+    # child个数始终为2个
+    # train_data表示该节点上的训练数据
+
     def __init__(self):
         TreeNode.__init__(self)
-        
+        self.train_data = np.array([])
+        self.label = np.array([])
+    def predict(self, feature):
+        if len(self.child) > 0:
+            if feature[self.data[1]] > self.data[2]:
+                return self.child[1].predict(feature)
+            else:
+                return self.child[0].predict(feature)
+        return self.label
